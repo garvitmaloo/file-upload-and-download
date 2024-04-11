1ab1cc64059d7e6f9207c46c778fa906464b025a
Garvit <garvitmaloo.21@gmail.com>
2024-04-11 11:12:11 +0530

    Setup multer for uploading file(s)

---
+++ src/config/multer.ts
@@ -0,0 +1,45 @@
+import type { Request } from "express";
+import multer, { diskStorage } from "multer";
+import type { FileFilterCallback } from "multer";
+
+import {
+  ACCEPTED_FILE_TYPES,
+  MAX_MEDIUM_FILE_SIZE,
+  MAX_SMALL_FILE_SIZE
+} from "../utils/constants";
+
+const fileFilterHandler = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: FileFilterCallback
+): void => {
+  if (ACCEPTED_FILE_TYPES.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("This file format is not supported"));
+  }
+};
+
+const diskStorageConfig = diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, `${process.cwd()}/uploads`);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+export const singleUpload = multer({
+  storage: diskStorageConfig,
+  fileFilter: fileFilterHandler,
+  limits: { fileSize: MAX_MEDIUM_FILE_SIZE }
+});
+
+export const multipleUploads = multer({
+  storage: multer.memoryStorage(),
+  fileFilter: fileFilterHandler,
+  limits: {
+    files: 10,
+    fileSize: MAX_SMALL_FILE_SIZE
+  }
+});
---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export const ACCEPTED_FILE_TYPES = [
+  "image/png",
+  "image/jpg",
+  "image/jpeg",
+  "image/gif",
+  "image/webp",
+  "audio/mpeg",
+  "video/mp4",
+  "video/x-msvideo",
+  "video/webm",
+  "application/pdf",
+  "application/msword",
+  "application/vnd.ms-excel",
+  "application/vnd.ms-powerpoint",
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+];
+
+export const MAX_SMALL_FILE_SIZE = 10 * 1024 * 1024; // 10 MB
+
+export const MAX_MEDIUM_FILE_SIZE = 300 * 1024 * 1024; // 300 MB
