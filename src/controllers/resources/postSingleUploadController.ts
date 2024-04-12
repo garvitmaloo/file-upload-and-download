3374179859b285a15872949618f55f33829799f5
Garvit <garvitmaloo.21@gmail.com>
2024-04-12 21:57:09 +0530

    Fix single file upload bug

--- src/controllers/resources/postSingleUploadController.ts
+++ src/controllers/resources/postSingleUploadController.ts
@@ -1,4 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
+import fs from "fs";
 
 import uploadFileToS3 from "../../service/resources/uploadFileToS3";
 import { logger } from "../../utils/logger";
@@ -8,7 +9,9 @@ const postSingleUploadController = async (
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  const file = req.file;
+  const file = req.file!;
+  const path = `${process.cwd()}/uploads/${file.originalname}`;
+  const fileBuffer = fs.readFileSync(path);
 
   if (file === undefined) {
     res.statusCode = 400;
@@ -16,7 +19,7 @@ const postSingleUploadController = async (
     return;
   }
 
-  const response = await uploadFileToS3(file.originalname, file.buffer);
+  const response = await uploadFileToS3(file.originalname, fileBuffer);
 
   if (response.error !== null) {
     logger.error("Error uploading file to S3", response.error.message);
